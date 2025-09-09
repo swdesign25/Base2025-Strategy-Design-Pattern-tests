5ca0f20ab01025771fb811f76fdece6fb26a7e7d
Petteri Jekunen <petteri.jekunen@tuni.fi>
2025-09-09 16:34:39 +0300

    Use mvn for test 4

--- test4.sh
+++ test4.sh
@@ -2,6 +2,10 @@
 
 echo "Maven in t2-pattern-based-solution"
 
+echo " -> search for pox.xml"
 find t2-pattern-based-solution -name pom.xml -exec cat {} \;
 
+echo " -> run mvn validate"
+cd t2-pattern-based-solution
+mvn validate
 
