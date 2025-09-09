e4185df27a6d898895be87f25d89c45591d257e5
Petteri Jekunen <petteri.jekunen@tuni.fi>
2025-09-09 16:44:31 +0300

    Run mvn validate also for basic folder

--- test2.sh
+++ test2.sh
@@ -2,6 +2,10 @@
 
 echo "Maven in t1-folder"
 
+echo " -> search for pox.xml"
 find t1-basic-solution -name pom.xml -exec cat {} \;
 
+echo " -> run mvn validate"
+cd t1-basic-solution 
+mvn validate
 
